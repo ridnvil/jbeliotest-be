a31131a568fa4231f599f56c8b36052502ad2a6f
Ridwan <rid.nvil17@gmail.com>
2025-06-27 01:02:07 +0700

    add open telemetry

--- pkg/middleware/otelmiddleware.go
+++ pkg/middleware/otelmiddleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/codes"
 )
@@ -9,12 +10,14 @@ import (
 func OpenTelemetryMiddleware() fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		tracer := otel.Tracer("myapp")
+		log.Info(c.Context(), c.Method(), c.Path(), c.IP())
 		_, span := tracer.Start(c.Context(), c.Method()+" "+c.Path())
 		c.Locals("otel-span", span)
 		defer span.End()
 		err := c.Next()
 		if err != nil {
 			span.SetStatus(codes.Error, err.Error())
+			log.Error(c.Context(), c.Method()+" "+c.Path(), err)
 		} else {
 			span.SetStatus(codes.Ok, "OK")
 		}
